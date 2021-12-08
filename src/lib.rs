ae5a210922c202e88cc97bac377e50f52ac559f3
rahulc29 <rahul29112002@gmail.com>
2021-12-08 21:29:43 +0530

    Add tests for uniform generation for LCGs

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ mod seed_factory;
 mod tests {
     use std::thread;
     use std::time::Duration;
+    use crate::generator::Generator;
+    use crate::linear_congruential_generator::{bsd_generator, microsoft_generator};
     use crate::seed_factory;
 
     #[test]
@@ -27,5 +29,21 @@ mod tests {
         let seed_a = factory.create_seed();
         let seed_b = factory.create_seed();
         assert_ne!(seed_a, seed_b);
+    }
+
+    impl dyn Generator {
+        fn test_gen_uniform(&mut self) {
+            let uniform = self.gen_uniform();
+            assert!(uniform < 1f32);
+            assert!(uniform >= 0f32);
+        }
+    }
+
+    #[test]
+    fn lcg_gen_uniform() {
+        let mut microsoft = microsoft_generator();
+        microsoft.test_gen_uniform();
+        let mut bsd = bsd_generator();
+        bsd.test_gen_uniform();
     }
 }
